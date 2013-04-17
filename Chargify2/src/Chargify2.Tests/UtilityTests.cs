ebce2579f5467b73ac80d958cf5c3783950c9d72
Kori Francis <byter@me.com>
2013-04-17 00:45:49 -0400

    Add initial tests

---
+++ Chargify2/src/Chargify2.Tests/UtilityTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chargify2.Tests
+{
+    [TestClass]
+    public class UtilityTests
+    {
+        [TestMethod]
+        public void Can_Calculate_Valid_Signature()
+        {
+            // Arrange
+            string message = "test123";
+            string secret = "secret";
+
+            // Act
+            var calculatedSignature = message.CalculateSignature(secret);
+
+            // Assert
+            Assert.IsNotNull(calculatedSignature);
+            Assert.IsInstanceOfType(calculatedSignature, typeof(string));
+            Assert.IsTrue(calculatedSignature.Length > 0);
+            Assert.IsTrue(Encoding.ASCII.GetByteCount(calculatedSignature) == 40);
+        }
+
+        [TestMethod]
+        public void Can_Get_QueryString_From_Hashtable()
+        {
+            // Arrange
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("key1", "value1");
+            hashtable.Add("key2", "value2");
+            hashtable.Add("key3", "value3");
+
+            // Act
+            var queryString = hashtable.ToQueryString();
+
+            // Assert
+            Assert.IsNotNull(queryString);
+            Assert.IsInstanceOfType(queryString, typeof(string));
+            Assert.IsTrue(queryString.Contains("key1=value1"));
+            Assert.IsTrue(queryString.Contains("key2=value2"));
+            Assert.IsTrue(queryString.Contains("key3=value3"));
+        }
+    }
+}
